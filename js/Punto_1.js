42b1b03717b57aa1b0a4db95d98780c1be925a29
Joan Romero <joan.romero@carval.com.co>
2021-09-05 15:32:55 -0500

    Primer Punto

---
+++ js/Punto_1.js
@@ -0,0 +1,9 @@
+
+function mostrar_salario(){
+
+    let horas=document.getElementById("horas").value;
+    let costo=document.getElementById("costo").value;
+
+    console.log(horas * costo);  
+}
+
